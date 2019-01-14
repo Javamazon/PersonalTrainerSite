2fd6edebb430c366d371cfad6bf48150e22632fd
Lindsay R Fletcher <40005868+Javamazon@users.noreply.github.com>
2019-01-14 14:27:26 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -9,17 +9,21 @@ function myFunction() {
 
 
 var theForm = document.forms['calorieForm'];
-var gender = '';
-
 
 
 function calculateTotal() {
+ 
 
   getGender();
   console.log(gender);
 
+  var formHeight = calcHeight();
+  var formAge = getAge();
+  var formWeight = getWeight();
 
-
+  console.log(formHeight);
+  console.log(formAge);
+  console.log(formWeight);
 
 
 
@@ -38,11 +42,47 @@ function calculateTotal() {
     console.log(gender);
   }
 
+
+
   function calcHeight() {
-    
+    var i = document.getElementById("inches");
+    var inches = i.options[i.selectedIndex].text;
+
+    var feet = document.getElementById("feet");
+    var height = feet.options[feet.selectedIndex].text;
+
+
+    height = parseInt(height);
+    inches = parseInt(inches);
+
+    var totalInches = height * 12;
+    totalInches = totalInches + inches;
+
+
+    console.log(totalInches);
+    return totalInches;
   }
 
-};
+  function getAge() {
+    var ageInput = document.getElementById('age');
+    ageInput = ageInput.value;
+    ageInput = parseInt(ageInput);
+    return ageInput;
+
+  }
+
+  function getWeight() {
+    var weightInput = document.getElementById('weight');
+    weightInput = weightInput.value;
+    weightInput = parseInt(weightInput);
+
+    console.log(weightInput);
+    return weightInput;
+  }
+
+
+
+}
 
 
 
